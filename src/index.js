8df5ff4d19fd25020e4ffa9cb10640e5e6ca06e2
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-02-12 14:19:16 +1100

    Changed structure to handle redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+
+import "./index.css";
+import App from "./components/app";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// NPM INSTALLS
+// Axios is a promise based HTTP client
+// Lodash is a JS utility library, we will use this to to deal witht he data from the api
+// Redux-Promise is a library to deal with promises in our "action" creators
+// React-Redux is a state container for React
